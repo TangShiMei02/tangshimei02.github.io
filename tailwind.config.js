d843fe8de37c2f31f38449a70b8ecd64143a1e41
TangShiMei <2244182951@qq.com>
2025-08-10 16:36:07 +0800

    Phase 1: Modern Setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#667eea',
+        secondary: '#ff7a7a',
+      },
+      fontFamily: {
+        handwritten: ['"Comic Neue"', 'cursive'],
+      }
+    },
+  },
+  plugins: [],
+}
