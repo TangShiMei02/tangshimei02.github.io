caaaebb0962c85cd657e3a2baf663c96d3ae0e2d
TangShiMei <2244182951@qq.com>
2025-08-10 17:25:06 +0800

    2025 0810  1725

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App'; // 引入你的组件文件
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />, document.getElementById('root')
+);
