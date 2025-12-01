f5688f581bcd929a7b26f22975896945115473f5
TangShiMei02 <2244182951@qq.com>
2025-12-01 21:09:57 +0800

    2025 1201 21 09 更新 大改！

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App'; // 引入你的组件文件
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />, document.getElementById('root')
-);
